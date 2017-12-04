3d946c1f40e6b2a2f588f5d4c1b806a29d69360f
Carlos Sanchez Marin <casamatechs@gmail.com>
2017-12-03 22:39:24 -0600

    Little main for the managers

--- src/edu/troy/cs/Main.java
+++ src/edu/troy/cs/Main.java
@@ -3,19 +3,42 @@ package edu.troy.cs;
 import edu.troy.cs.exceptions.StudentConnectionException;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[]args){
-        try {
-            Student trial_st = new Student(1532197, "Carlos", "Sanchez", 'M', 23, new BigDecimal(3.87), Year.SENIOR, false, false, "c4rl0s", "csanchez@troy.edu");
-            //String[] pff = {"PAC", "NRH", "NEW", "TV", "HAM"};
-            //trial_st.doReservation(pff);
-            Manager manager = new Manager();
-            manager.assignRooms();
-            //manager.getPuntuationToAll();
-        } catch (StudentConnectionException e) {
-            System.err.println("Failure during DB connection");
+    public static void main(String[] args) throws StudentConnectionException {
+
+        Manager manager = new Manager();
+        Scanner scn = new Scanner(System.in);
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("What do you want to do?");
+            System.out.println("[1] Calculate the puntuation for one student");
+            System.out.println("[2] Calculate the puntuation for all the students");
+            System.out.println("[3] Assign dorms");
+            System.out.println("[4] Exit");
+            int opt = scn.nextInt();
+            switch (opt) {
+                case 1:
+                    System.out.println("Write the student ID");
+                    int idS = scn.nextInt();
+                    int punt = manager.getPuntuation(idS);
+                    System.out.println("The student " + idS + " has a puntuation of " + punt + ".\n\n\n");
+                    break;
+                case 2:
+                    manager.getPuntuationToAll();
+                    System.out.println("The puntuation has been calculated for all the students.\n\n\n");
+                    break;
+                case 3:
+                    manager.assignRooms();
+                    System.out.println("Dorms assigned\n\n\n");
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+            }
         }
+        System.exit(0);
     }
 }
